b817154537c3e172f4b08c3369f0dae63dfaf76c
Tetsuro Takao <tetsuro.takao@processtune.com>
2022-06-27 10:25:11 +0900

    Initial commit

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// using var channel = GrpcChannel.ForAddress("https://localhost:7082");
+using var channel = GrpcChannel.ForAddress("https://localhost:8585");
+// using var channel = GrpcChannel.ForAddress($"https://grpcprocesstuneeastus2.azurewebsites.net");
+
+var client = new Greeter.GreeterClient(channel);
+
+// Metadata headers = new()
+// {
+//     { "Alt-Svc", "h3=\":8585\"" }
+// };
+
+// var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" },headers);
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
